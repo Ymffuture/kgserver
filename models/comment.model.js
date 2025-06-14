53f142ea321e85c58cbe09a99fb2f2e91d03b653
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-14 10:25:50 +0200

    Update comment.model.js

--- models/comment.model.js
+++ models/comment.model.js
@@ -4,28 +4,50 @@ const commentSchema = new mongoose.Schema(
   {
     content: {
       type: String,
-      required: true,
+      required: [true, 'Comment content is required'],
+      trim: true,
+      minlength: 1,
+      maxlength: 1000
     },
     postId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Blog'
+      ref: 'Blog',
+      required: true
     },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
+      ref: 'User',
+      required: true
     },
-    likes: {
-      type: Array,
-      default: [],
+    parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment', // Support for nested replies
+      default: null
     },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
     numberOfLikes: {
       type: Number,
-      default: 0,
+      default: 0
     },
+    isEdited: {
+      type: Boolean,
+      default: false
+    }
   },
   { timestamps: true }
 );
 
-const Comment = mongoose.model('Comment', commentSchema);
+// Optional: Auto-update `numberOfLikes` on like/unlike
+commentSchema.methods.updateLikesCount = function () {
+  this.numberOfLikes = this.likes.length;
+  return this.save();
+};
 
+const Comment = mongoose.model('Comment', commentSchema);
 export default Comment;
+
