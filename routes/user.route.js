fdabe1c0332b317ecefb9731232c85d98200b304
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-16 09:17:17 +0200

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ import express from "express"
 import { getAllUsers, login, logout, register, updateProfile } from "../controllers/user.controller.js"
 import { isAuthenticated } from "../middleware/isAuthenticated.js"
 import { singleUpload } from "../middleware/multer.js"
-
+import {googleLogin} from "../controllers/auth.js" 
 const router = express.Router()
 
 router.route("/register").post(register)
@@ -10,5 +10,6 @@ router.route("/login").post(login)
 router.route("/logout").get(logout)
 router.route("/profile/update").put(isAuthenticated, singleUpload, updateProfile)
 router.get('/all-users', getAllUsers);
+router.route('/auth/google-login').post(googleLogin);
 
 export default router;
