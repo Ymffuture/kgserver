9c8399d71e59ce40db8cd8223bbe3345cd6bd371
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-16 07:40:38 +0200

    Update blog.model.js

--- models/blog.model.js
+++ models/blog.model.js
@@ -1,37 +1,48 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    subtitle: {
-        type: String,
-        // required: true
-    },
-    description: {
-        type: String,
-        // required:true
-    },
-    thumbnail: {
-        type: String,
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+  title: {
+    type: String,
+    required: true
+  },
+  subtitle: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  thumbnail: {
+    type: String,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  category: {
+    type: String
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+    dislikes: [{
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'User'
+}],
 
-    },
-    category: {
-        type: String
-    },
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-    isPublished:{
-        type:Boolean,
-        default:false
-    }
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  isPublished: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
 
+// Ensure a user can like a blog only once
+blogSchema.index({ _id: 1, 'likes': 1 });
 
-}, { timestamps: true })
+export const Blog = mongoose.model("Blog", blogSchema);
 
-export const Blog = mongoose.model("Blog", blogSchema)
