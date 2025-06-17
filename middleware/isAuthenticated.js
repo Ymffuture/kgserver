915ab3618402746b749fd692b81ed8afa50fe959
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-17 16:26:43 +0200

    Update isAuthenticated.js

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,16 +1,17 @@
-
-import jwt from "jsonwebtoken";
+// middlewares/auth.js
+import jwt from 'jsonwebtoken';
 
 export const isAuthenticated = (req, res, next) => {
   const token = req.cookies.token;
-  if (!token) return res.status(401).json({ success: false, message: "Not authorized" });
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req.id = decoded.userId;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.id = decoded.id; // <-- Required!
+    req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ success: false, message: "Invalid token" });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
