b293b7b01705103dd654905a2f6cbec97f474681
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-18 02:23:53 +0200

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "quorvex", // ✅ Use string here
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB connected successfully to 'quorvexinstitute'");
   } catch (error) {
