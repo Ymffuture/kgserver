f6003093180290dc4e238f3e306df1f85f1d2792
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-09-25 06:25:02 +0200

    Update isAuthenticated.js

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,20 +1,27 @@
-// middleware/auth.js
-import jwt from "jsonwebtoken";
-
-export const isAuthenticated = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "You are not authenticated" });
-  }
-
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.id = decoded.id;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-};
+import jwt from "jsonwebtoken"
 
 
+export const isAuthenticated = async (req, res, next) =>{
+    try {
+        const token = req.cookies.token;
+        
+        if(!token){
+            return res.status(401).json({
+                message:"Note:you are not authenticated",
+                success:false,
+            })
+        }
+        const decode =  jwt.verify(token, process.env.SECRET_KEY)
+        if(!decode){
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false,
+            })
+        }
+        req.id = decode.userId;
+        next();
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
